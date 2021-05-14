efede203b21250701e8917e5d8775591b213717f
memcmahon <mcmahon.meganelizabeth@gmail.com>
2021-05-14 10:40:21 -0600

    completes iteration2

--- lib/classroom.rb
+++ lib/classroom.rb
@@ -6,4 +6,20 @@ class Classroom
     @capacity = capacity
     @students = []
   end
+
+  def add_student(student_name)
+    @students << student_name
+  end
+
+  def yell_at_students
+    @students.map do |student|
+      student.upcase
+    end
+
+    # upcased_names = []
+    # @students.each do |student|
+    #   upcased_names << student.upcase
+    # end
+    # upcased_names
+  end
 end
